07b663516850eb1bcfd5c29c2aefc6882f6ffcde
henokmulat <henokmulat27@gmail.com>
2025-11-07 14:36:34 +0300

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">
-        Welcome to Next js 👋
+      <h1 className="text-3xl ">Welcome to Next js 👋</h1>
+      <h1 className="text-3xl font-inter">Welcome to Next js (Inter) 👋</h1>
+      <h1 className="text-3xl font-space-grotesk">
+        Welcome to Next js (Space Grotesk) 👋
       </h1>
     </div>
   );
