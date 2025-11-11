6cfef06a71d031c901d260074a1a5192673c2a6d
henokmulat <henokmulat27@gmail.com>
2025-11-11 16:36:54 +0300

    add new pages for AskAQuestion, Collections, Community, FindJobs, Profile, and Tags

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -9,15 +6,6 @@ const Home = async () => {
   return (
     <div>
       <h1 className="text-3xl ">Welcome to Next js 👋</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Logout</Button>
-      </form>
     </div>
   );
 };
