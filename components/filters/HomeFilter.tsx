9fe4f3d6d6b677d002ada8d30cf7e87147d92d0c
henokmulat <henokmulat27@gmail.com>
2025-11-19 22:36:01 +0300

    refactor HomeFilter component to improve filter handling and URL query management

--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -1,26 +1,41 @@
 "use client";
 import { useState } from "react";
 import { Button } from "../ui/button";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { formUrlQuery, removeKeysFormUrlQuery } from "@/lib/url";
 
 const filters = [
-  { name: "Newest", value: "newest" },
-  { name: "Popular", value: "popular" },
-  { name: "Unanswered", value: "unanswered" },
-  { name: "Recommended", value: "recommended" },
+  { name: "React", value: "react" },
+  { name: "Javascript", value: "javascript" },
+  // { name: "Newest", value: "newest" },
+  // { name: "Popular", value: "popular" },
+  // { name: "Unanswered", value: "unanswered" },
+  // { name: "Recommended", value: "recommended" },
 ];
 const HomeFilter = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const filterParams = searchParams.get("filter");
   const [active, setActive] = useState(filterParams || "");
 
   const handleTypeClick = (filter: string) => {
-    if (filter) {
-      setActive(filter);
-    } else {
+    let newUrl = "";
+    if (filter === active) {
       setActive("");
+      newUrl = removeKeysFormUrlQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["filter"],
+      });
+    } else {
+      setActive(filter);
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: filter.toLowerCase(),
+      });
     }
+    router.push(newUrl, { scroll: false });
   };
   return (
     <div className="mt-10 hidden flex-wrap gap-3 sm:flex">
