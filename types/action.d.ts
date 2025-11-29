8d4a67491a1dea2732972b02f5a4d702ae2877bd
henokmulat <henokmulat27@gmail.com>
2025-11-29 20:59:48 +0300

    Define SignInWithOAuthParams interface for OAuth sign-in request parameters.

---
+++ types/action.d.ts
@@ -0,0 +1,10 @@
+export interface SignInWithOAuthParams {
+  provider: "github" | "google";
+  providerAccountId: string;
+  user: {
+    name: string;
+    image: string;
+    email: string;
+    username: string;
+  };
+}
